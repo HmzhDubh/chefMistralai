4f916f1efebfdb5996d990522e89e117f87f2136
HmzhDubh <meezo.2015@hotmail.com>
2025-03-12 11:00:18 +0300

    AI powered recipe generator using react

---
+++ src/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return (
+        <header className="header">
+            <img src='https://github.com/scrimba/learn-react/blob/main/03.%20React%20State/02.%20Chef%20Claude%20-%20Header/images/chef-claude-icon.png?raw=true' alt='chef claude logo'></img>
+            <h1> Chef Mistral </h1>
+        </header>
+    )
+}
